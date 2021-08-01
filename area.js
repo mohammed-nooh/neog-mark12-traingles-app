47827361646ab06ec20149d50550c5925df744da
mohammed-nooh <83730829+mohammed-nooh@users.noreply.github.com>
2021-08-02 00:54:00 +0530

    feat : added area2 checker

--- area.js
+++ area.js
@@ -6,8 +6,8 @@ const area1=document.querySelector("#area1ID");
 const area2=document.querySelector("#area2ID");
 const form1=document.querySelector(".form1");
 const form2=document.querySelector(".form2");
-const inputs1=document.querySelectorAll(".hypo-input");
-const inputs2=document.querySelectorAll(".hypo-input");
+const inputs1=document.querySelectorAll(".hypo-input1");
+const inputs2=document.querySelectorAll(".hypo-input2");
 const message1=document.querySelector("#area-outputID1");
 const message2=document.querySelector("#area-outputID2");
 
@@ -23,9 +23,9 @@ area1Btn.addEventListener("click",()=>{
         const b=Number(inputs1[0].value);
         const h=Number(inputs1[1].value);
 
-        let area=0;
-        area=0.5*b*h;
-        message1.innerText=`Area = ${area}`;
+        let area1=0;
+        area1=0.5*b*h;
+        message1.innerText=`Area = ${area1}`;
 
     })
 
@@ -36,6 +36,26 @@ area1Btn.addEventListener("click",()=>{
 area2Btn.addEventListener("click",()=>{
     area1.classList.add("hidden");
     area2.classList.remove("hidden");
+
+    form2.addEventListener("submit",(e)=>{
+        e.preventDefault();
+
+        const a=Number(inputs2[0].value);
+        const b=Number(inputs2[1].value);
+        const c=Number(inputs2[2].value);
+        console.log(a,b,c);
+
+        if((a+b)>c && (b+c)>a && (a+c)>b){
+            const s=(a+b+c)/2;
+            const area2=Math.sqrt(s*(s-a)*(s-b)*(s-c));
+            message2.innerText=`Area = ${area2}`;
+        }
+        else{
+            message2.innerText=`Invalid numbers`;
+        }
+
+    })
+
 })
 
 
