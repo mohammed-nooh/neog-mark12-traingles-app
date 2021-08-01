16c8a10c5fed193bb1ba9a8fe411dcac8318f5fb
mohammed-nooh <83730829+mohammed-nooh@users.noreply.github.com>
2021-08-01 22:08:18 +0530

    created hypo page

--- anglesTri.js
+++ anglesTri.js
@@ -1,3 +1,34 @@
 "use strict";
 
 
+const inputs=document.querySelectorAll("#inputID");
+const submitBtn=document.querySelector("#submit-sideID");
+const form=document.querySelector("#form-tri");
+const message=document.querySelector("#outputID");
+
+function messageHandler(text){
+    message.innerText=text;
+}
+
+function formHandler(event){
+    event.preventDefault();
+    
+    const a=Number(inputs[0].value);
+    const b=Number(inputs[1].value);
+    const c=Number(inputs[2].value);
+    // console.log(a,b,c);
+    let sum=0;
+    sum=a+b+c;
+    if(sum===180){
+        messageHandler("Yay! These angles can make a Triangle");
+
+    }
+    else{
+        messageHandler("OOPS! These angles cannot make a triangle");
+        
+    }
+
+}
+
+
+form.addEventListener("submit",formHandler)
